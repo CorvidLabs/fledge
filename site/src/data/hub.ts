212c591e4bd53856667f1bcaf20bdc08aad80e43
Leif <leif.algo@pm.me>
2026-06-14 10:47:22 -0600

    Update redirect URLs for marketing and docs (#422)

--- site/src/data/hub.ts
+++ site/src/data/hub.ts
@@ -5,5 +5,5 @@
  * migrated to the CorvidLabs hub. These constants are the redirect targets used
  * by every route on this (now redirect-only) site.
  */
-export const HUB_MARKETING = 'https://corvidlabs.github.io/corvidlabs-site/fledge/'
-export const HUB_DOCS = 'https://corvidlabs.github.io/corvidlabs-site/fledge/docs/'
+export const HUB_MARKETING = 'https://corvidlabs.xyz/fledge/'
+export const HUB_DOCS = 'https://corvidlabs.xyz/fledge/docs/'
