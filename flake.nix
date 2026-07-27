06c14e73d71f0ad62bdf3545440d2e9da015d7e3
0xLeif <leif.algo@pm.me>
2026-07-27 14:36:28 -0600

    chore: release v1.7.2

--- flake.nix
+++ flake.nix
@@ -14,7 +14,7 @@
       {
         packages.default = pkgs.rustPlatform.buildRustPackage {
           pname = "fledge";
-          version = "1.7.1";
+          version = "1.7.2";
           src = self;
 
           cargoLock = {
